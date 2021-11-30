9135df8560b219307b8a17bddb0e0f76d8b9b5f3
JuYeon Lee <cavss1118@gmail.com>
2021-11-30 14:59:36 +0900

    #11 AdBlueMapPinView // 1. 아이템 클릭 리스너 추가 2. 전화번호 클릭시 전화걸기 인텐트로 이동 3. 다지안 다시

---
+++ app/src/main/java/com/cavss/carsupporter/ui/view/views/adblue/mapview/AdBlueMapPinView.kt
@@ -0,0 +1,16 @@
+package com.cavss.carsupporter.ui.view.views.adblue.mapview
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import com.cavss.carsupporter.model.adblue.AdBlueModel
+import com.google.android.gms.maps.GoogleMap
+
+@Composable
+fun AdBlueMapPinView(
+    map : GoogleMap,
+    model : AdBlueModel
+) {
+    val context = LocalContext.current
+
+
+}
