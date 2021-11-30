f9607d650bd87cb3af26f66670998353eabf1daa
JuYeon Lee <cavss1118@gmail.com>
2021-11-30 15:04:45 +0900

    #16 AdView // 1. refactoring 필요 2. livedata 대신 flow나 state 적용시도

--- app/src/main/java/com/cavss/carsupporter/vm/AdBlueVM.kt
+++ app/src/main/java/com/cavss/carsupporter/vm/AdBlueVM.kt
@@ -1,70 +1,48 @@
 package com.cavss.carsupporter.vm
 
-import android.location.Location
-import android.os.CountDownTimer
 import android.util.Log
+import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.cavss.carsupporter.backend.retrofit2.RetrofitManager
 import com.cavss.carsupporter.model.adblue.AdBlueModel
-import com.cavss.carsupporter.repository.AdBlueRepository
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.launch
 
 class AdBlueVM : ViewModel() {
     private val _adBlueList = MutableLiveData<List<AdBlueModel>>()
-    private fun setAdBlueList(list : List<AdBlueModel>){
-        _adBlueList.value = list.sortedByDescending { it.stock }
+    fun setAdBlueList(list : List<AdBlueModel>){
+        _adBlueList.value = list
+            .filterNot {
+                it.stock == 0.0
+            }
     }
     val getAdBlueList : LiveData<List<AdBlueModel>>
         get() = _adBlueList
 
-    private fun getAdBlueListByRetrofit(){
+    private val _text = mutableStateOf("")
+    fun setText(text : String) {
+        _text.value = text
+        try {
+            val newList =  _adBlueList.value?.filter { it.address.contains(text) } ?: listOf()
+            setAdBlueList(list = newList)
+            Log.e("mException", "AdBlueVM, text : ${_text}setText : ${newList}")
+        }catch (e:Exception){
+            Log.e("mException", "AdBlueVM, setText // Exception : ${e.message}")
+        }
+    }
+    val getText : State<String>
+        get() = _text
+
+    private val _location = MutableLiveData(mapOf("lati" to 0.0, "long" to 0.0))
+    fun setLocation(position : Map<String, Double>) { _location.postValue(position) }
+    val getLocation : LiveData<Map<String, Double>>
+        get() = _location
+
+    init {
         RetrofitManager.instance.getDefShopList {
             setAdBlueList(list = it)
         }
-    }
-
-    private val _text = MutableLiveData<String>("zzzz")
-    fun setText(text : String) { _text.postValue(text) }
-    val getText : LiveData<String>
-        get() = _text
-
-    val location = MutableStateFlow<Location>(getCurrentLocation(0.0, 0.0))
-    val addressText = mutableStateOf("")
-    var isMapEditable = mutableStateOf(true)
-    var timer: CountDownTimer? = null
-
-    fun getCurrentLocation(
-        latitude : Double,
-        longitude : Double
-    ) : Location {
-        val initialLocation = Location("")
-        initialLocation.latitude = latitude
-        initialLocation.longitude = longitude
-        return initialLocation
-    }
-
-    fun updateLocation(latitude: Double, longitude: Double){
-        if(latitude != location.value.latitude) {
-            val location = Location("")
-            location.latitude = latitude
-            location.longitude = longitude
-            setLocation(location)
-        }
-    }
-
-    fun setLocation(loc: Location) {
-        location.value = loc
-    }
-
-
-
-    init {
-        getAdBlueListByRetrofit()
     }
 
 }
