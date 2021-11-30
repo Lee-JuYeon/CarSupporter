fdcd0ee83969b2839e9f702ab1aed384a8333cc0
JuYeon Lee <cavss1118@gmail.com>
2021-11-30 15:03:54 +0900

    #16 AdView // 1. key 변경 2. 리스트에 광고 추가 3. 네이티브, 전명광고, 앱실행시 광고 추가

--- app/src/main/java/com/cavss/carsupporter/ui/view/custom/adview/AdView.kt
+++ app/src/main/java/com/cavss/carsupporter/ui/view/custom/adview/AdView.kt
@@ -1,19 +1,9 @@
 package com.cavss.carsupporter.ui.view.custom.adview
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalInspectionMode
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.LifecycleOwner
 import com.cavss.carsupporter.R
@@ -43,7 +33,7 @@ fun AdView(type : AdMobType) {
                 factory = { context ->
                     AdView(context).apply {
                         adSize = AdSize.BANNER
-                        adUnitId = R.string.admob_key_dummy_def.toString()
+                        adUnitId = R.string.admob_banner.toString()
 
                         loadAd(AdRequest.Builder().build())
                     }
